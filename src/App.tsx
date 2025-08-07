461509c0784267311ea89a26a23c59adf863d597
Herb Segismar <herbert.segismar@yahoo.com>
2025-08-07 18:24:26 +1200

    errors

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import RootLayout from "./components/RootLayout";
 import Home from "./components/Home";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
-import NotFound from "./components/NotFound";
+import NotFound from "./components/_NotFound";
 
 function App() {
   const router = createBrowserRouter(
